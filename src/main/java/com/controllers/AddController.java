a334542d3566367c5431745cb505ccd246eebdee
Ehtesham <ehteshamuddinqureshi.mohd@zemosolabs.com>
2022-02-02 01:06:39 +0530

    Annotations Based Configuration with RequestParams

--- src/main/java/com/controllers/AddController.java
+++ src/main/java/com/controllers/AddController.java
@@ -4,6 +4,7 @@ import com.service.AddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class AddController {
 @RequestMapping("/add")
-    public ModelAndView add(HttpServletRequest request , HttpServletResponse response){
+    public ModelAndView add(@RequestParam("t1") int t1 , @RequestParam("t2") int t2){
     ModelAndView modelAndView = new ModelAndView();
-    int t1 = Integer.parseInt(request.getParameter("t1"));
-    int t2 = Integer.parseInt(request.getParameter("t2"));
     int sum = new AddService().add(t1 , t2);
     modelAndView.setViewName("display");
     modelAndView.addObject("sum" , sum);
